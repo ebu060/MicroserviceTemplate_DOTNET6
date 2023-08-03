a4ffa03f785f585a9c45e117cbda777c87d8dc4e
Ebubekir YARDIMCI <ebubekir.yrd@gmail.com>
2023-08-03 17:52:02 +0300

    Add project files.

---
+++ Catalog.Infrastructure/Data/ProductContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public class ProductContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool CheckProducts = productCollection.Find(b => true).Any();
+            string path = Path.Combine("Data", "SeedData", "products.json");
+            if (!CheckProducts)
+            {
+                var productsData = File.ReadAllText(path);
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                if (products != null)
+                {
+                    foreach (var type in products)
+                    {
+                        productCollection.InsertOneAsync(type);
+                    }
+                }
+            }
+        }
+    }
+}
