e40f270e0813059cef3162926b0c7699f65738cf
ebubekir yardımcı <ebubekir_yrd@hotmail.com>
2023-09-13 18:22:49 +0300

    Repository implementation started.

--- Catalog.Infrastructure/Repositories/ProductRepository.cs
+++ Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,61 @@ using System.Threading.Tasks;
 
 namespace Catalog.Infrastructure.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypesRepository
     {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _context = catalogContext;
+        }
+
+        public async Task<Product> CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByBrand(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Brands.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public Task<Product> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
