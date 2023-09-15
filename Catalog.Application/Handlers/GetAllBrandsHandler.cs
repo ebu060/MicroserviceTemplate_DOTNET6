2707e2b439409760637bd9c614ca2f2071008544
Ebubekir Yardımcı <ebubekir.yrd@gmail.com>
2023-09-15 12:04:42 +0300

    CQRS mapper and handler added.

--- Catalog.Application/Handlers/GetAllBrandsHandler.cs
+++ Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
 
 namespace Catalog.Application.Handlers
 {
-    internal class GetAllBrandsHandler
-    {
-    }
+	public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+	{
+		private readonly IBrandRepository _brandRepository;
+		private readonly IMapper _mapper;
+
+		public GetAllBrandsHandler(IBrandRepository brandRepository,IMapper mapper)
+		{
+			_brandRepository = brandRepository;
+			_mapper = mapper;
+		}
+		public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+		{
+			var brandList = await _brandRepository.GetAllBrands();
+			var brandResponseList = _mapper.Map<IList<BrandResponse>>(brandList);
+			return brandResponseList;
+		}
+	}
 }
